12e206a22bc5d173fe65db0b73ea49b95ed0e1b9
youssefsahli <sahli.youssef@ymail.com>
2020-12-17 16:50:03 +0100

    initial

---
+++ index.rkt
@@ -0,0 +1,21 @@
+#lang scribble/base
+
+@(require scribble/core
+          scribble/html-properties)
+
+@title{Pharmacologie}
+@external-image
+@(define external-image
+   (elem
+    #:style
+    (style #f
+           (list (alt-tag "img")
+                 (attributes
+                  '((src . "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Constant_tempertature_bath_for_isolated_organs_Wellcome_M0013241.jpg/800px-Constant_tempertature_bath_for_isolated_organs_Wellcome_M0013241.jpg")))))))
+
+@author{Youssef Sahli}
+@(table-of-contents)
+@section{Antifongiques}
+@para{
+      Les antifongiques peuvent etre systemiques ou locaux (visant le systeme digestif)
+      De nombreuses familles font partie de cette classe pharmacologique}
