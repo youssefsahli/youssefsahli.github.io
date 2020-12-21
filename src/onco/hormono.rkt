9dc983e54062fe3fac6364fb657382b6f0a04602
youssefsahli <sahli.youssef@ymail.com>
2020-12-21 18:11:16 +0100

    constructing

---
+++ src/onco/hormono.rkt
@@ -0,0 +1,12 @@
+#lang scribble/manual
+@title{traitement hormonologique du cancer du sein}
+@section{Castration physique @emph{définitive}}
+@para{Elle se fait par chirurgie et radiotherapie localisée.}
+@section{Castration chimique, @emph{temporaire et réversible}}
+@para{Blocage de l'axe hypothalamo-hypophyso-gonadique pour empêcher la sécrétion hormonale}
+@itemlist[
+ @item{Analogues GnRH}
+ @item{Progestatifs
+ @itemlist[
+ @item{Médroxyprogestérone DEPO-PRODASONE}
+ @item{Acétate de Mégestrol MEGACE}]}]
